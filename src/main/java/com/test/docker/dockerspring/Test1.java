11c48b1e67325c1d5ff8e35b2d234d6040b2aa3c
aarvind3 <aarvind3@csc.com>
2018-08-10 16:21:36 +0530

    test

--- src/main/java/com/test/docker/dockerspring/Test1.java
+++ src/main/java/com/test/docker/dockerspring/Test1.java
@@ -2,7 +2,7 @@ class Test1{
 	public statis void main(){
 		System.out.println("Helo");
 		
-		System.out.println("Hello git hub demno ddfgsdfgsdfgsdfgsdfgsdfgsd");
+		System.out.println("H");
 		
 	}
 }
